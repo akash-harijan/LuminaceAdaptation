9fb70b8a4700593df59cc92f2db49eef19abbe70
Akash Harijan <akashdharani7@gmail.com>
2023-04-21 11:42:17 +0200

    combined all grpahs in one

--- graphs.m
+++ graphs.m
@@ -1,8 +1,9 @@
 clc;
 clear all;
 
-% data = readtable('Results/Giorgio/1,test.csv');
-data = readtable('test.csv');
+data = readtable('Results\Ahsan\test.csv');
+% data = readtable('Results/Giorgio/3-6-4-23-test.csv');
+% data = readtable('test.csv');
 
 
 colors = unique(data.Color);
@@ -11,12 +12,16 @@ color_names = ["White", "Red", "Green", "Blue", "Yellow", "Magenta", "Cyan"];
 % color_names	= ["Blue", "Yellow"];
 % test = [4 5];
 
+figure()
+t = tiledlayout(3,3);
+
 for i=1:length(colors)
     disp(colors(i));
     graph_data = data.Surrounding(data.Color==colors(i));
 
     
-    figure()
+%     figure()
+    nexttile
     plot(graph_data, '-o')
     title('Circles and Lines')
     title(color_names(i))
